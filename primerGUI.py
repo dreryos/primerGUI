65dce583bb141b93a023442b884bc259f8794828
dreryos <marek.foldy@gmail.com>
2021-05-04 11:56:09 +0200

    added license header

--- primerGUI.py
+++ primerGUI.py
@@ -1,3 +1,6 @@
+#primerGUI is free software: you can redistribute it and/or modify it under the terms of the GNU Lesser General Public License as published by the Free Software Foundation, either version 3 of the License, or (at your option) any later version.
+#You should have received a copy of the GNU Lesser General Public License along with primerGUI. If not, see http://www.gnu.org/licenses/.
+
 import sys
 import re
 from PySide6 import QtWidgets
